0fbe5f61b685fc5659bfebf59ca716d273c9ccf1
RahulSondhi <rahul.sondhi@hotmail.com>
2017-04-28 23:28:01 -0400

    Added My Favorite Things Tab

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,10 @@
 $(function () {
     var sidebar = $('.sidebar');
-    var top = sidebar.offset().top + parseFloat(sidebar.css('height'));
-    var top2 = parseFloat(sidebar.css('height'));
+    var top = parseFloat(sidebar.css('height'));
 
     $(window).scroll(function (event) {
       var y = $(this).scrollTop();
-      if (y >= top2) {
+      if (y >= top) {
         sidebar.addClass('fixed');
       } else {
         sidebar.removeClass('fixed');
