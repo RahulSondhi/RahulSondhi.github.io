99411b82f7d4e3a16ea85c68e738e5d0a48fc525
RahulSondhi <rahul.sondhi@hotmail.com>
2017-09-04 03:35:53 -0400

    Finsihed up visuals

--- js/script.js
+++ js/script.js
@@ -1,26 +1,65 @@
 $(function () {
+
   $("#menuButton").on("click",function(){
     openMenu();
+  });
+
+  $("#menuContainer").mouseleave(function(){
+    closeMenu();
   });
 
   $("#contactButton").on("click",function(){
     openContact();
   });
 
+  $("#logo").on("click",function(){
+    setStage(0);
+  });
+
+  setSky();
+  setMenu();
 });
 
+options = ["About Me","Experience","Education","Volunteering","Skills","Awards","Projects","Contact Me"];
+
 function openMenu(){
-  console.log("Menu");
+  $("#menuButtonContainer").css("visibility","hidden");
+  $(".menuOption").css("visibility","visible");
 }
 
 function openContact(){
-  console.log("Contact")
+  setStage(options.length-1);
 }
 
 function closeMenu(){
-  console.log("Close Menu");
+  $(".menuOption").css("visibility","hidden");
+  $("#menuButtonContainer").css("visibility","visible");
 }
 
-function closeContact(){
-  console.log("Close Contact");
+function setStage(index){
+  console.log(index);
+}
+
+function setSky(){
+  var time = new Date();
+  console.log(time.getHours());
+  if( time.getHours() > 19 ) {
+    $(".screen").css("background-color","#000");
+    $("body").css("background-color","#505050");
+  }else{
+    $(".screen").css("background-color","#87cefa");
+    $("body").css("background-color","#000");
+  }
+}
+
+function setMenu(){
+  for(var i = 0; i < options.length-1; i++){
+    var divToPush = $("<div></div>");
+    divToPush.attr("id", "option"+ i);
+    divToPush.attr("class", "menuOption button");
+    divToPush.html("<div class='menuText'>"+options[i]+"</div>");
+    divToPush.attr("onclick", "setStage("+i+")");
+    $("#menuContainer").append(divToPush);
+  }
+  $(".menuOption").css("visibility","hidden");
 }
