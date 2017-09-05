9ea53755419cc8aee1a67e9c5afe2b0d64e887a7
RahulSondhi <rahul.sondhi@hotmail.com>
2017-09-04 22:23:40 -0400

    Beginning on SVG Animation

--- js/script.js
+++ js/script.js
@@ -1,65 +1,152 @@
-$(function () {
+var options = ["About Me", "Experience", "Education", "Volunteering", "Skills", "Awards", "Projects", "Contact Me"];
+var menuAnimation = false;
+var index = 0;
 
-  $("#menuButton").on("click",function(){
+$(function() {
+
+  $("#menuButton").on("click", function() {
     openMenu();
   });
 
-  $("#menuContainer").mouseleave(function(){
+  $("#menuContainer").mouseleave(function() {
     closeMenu();
   });
 
-  $("#contactButton").on("click",function(){
+  $("#contactButton").on("click", function() {
     openContact();
   });
 
-  $("#logo").on("click",function(){
-    setStage(0);
+  $("#logo").on("click", function() {
+    stageIncrement(1);
   });
 
+  $("#leftArrow").on("click", function() {
+    stageIncrement(-1);
+  });
+
+  $("#rightArrow").on("click", function() {
+    stageIncrement(1);
+  });
+
+  setStage(index);
   setSky();
   setMenu();
 });
 
-options = ["About Me","Experience","Education","Volunteering","Skills","Awards","Projects","Contact Me"];
 
-function openMenu(){
-  $("#menuButtonContainer").css("visibility","hidden");
-  $(".menuOption").css("visibility","visible");
-}
-
-function openContact(){
-  setStage(options.length-1);
-}
-
-function closeMenu(){
-  $(".menuOption").css("visibility","hidden");
-  $("#menuButtonContainer").css("visibility","visible");
-}
-
-function setStage(index){
-  console.log(index);
-}
-
-function setSky(){
-  var time = new Date();
-  console.log(time.getHours());
-  if( time.getHours() > 19 ) {
-    $(".screen").css("background-color","#000");
-    $("body").css("background-color","#505050");
-  }else{
-    $(".screen").css("background-color","#87cefa");
-    $("body").css("background-color","#000");
+function openMenu() {
+  if (!menuAnimation) {
+    $("#menuButtonContainer").css("visibility", "hidden");
+    $(".menuOption").css("visibility", "visible");
+    $("#menuContainer").addClass("menuOpen");
+    menuAnimation = true;
+    window.setTimeout(function() {
+      $("#menuContainer").removeClass("menuOpen");
+      $("#menuContainer").css("left", "0");
+      menuAnimation = false;
+    }, 750);
   }
 }
 
-function setMenu(){
-  for(var i = 0; i < options.length-1; i++){
+function openContact() {
+  setStage(options.length);
+}
+
+function closeMenu() {
+  if (!menuAnimation) {
+    $("#menuContainer").addClass("menuClose");
+    menuAnimation = true;
+    window.setTimeout(function() {
+        $("#menuContainer").removeClass("menuClose");
+        $("#menuContainer").css("left", "-17%");
+        $(".menuOption").css("visibility", "hidden");
+        $("#menuButtonContainer").css("visibility", "visible");
+        menuAnimation = false;
+    }, 750);
+  }
+}
+
+function stageIncrement(increment){
+
+  var newIndex= index + increment;
+
+  if(newIndex <= options.length && newIndex > -1){
+    index = newIndex;
+  }else{
+    index = 0;
+  }
+
+  setStage(index);
+}
+
+function setStage(index) {
+  if(index == 0){
+    $("#skyContainer").css("visibility","visible");
+    $("#navigationContainer").css("visibility","hidden");
+  }else{
+    $("#skyContainer").css("visibility","hidden");
+    $("#navigationContainer").css("visibility","visible");
+  }
+  switch (index) {
+    case 0:
+      $("")
+      break;
+
+    case 1:
+
+      break;
+
+    case 2:
+
+      break;
+
+    case 3:
+
+      break;
+
+    case 4:
+
+      break;
+
+    case 5:
+
+      break;
+
+    case 6:
+
+    break;
+
+    case 7:
+
+      break;
+
+    default:
+      break;
+
+  }
+}
+
+function setSky() {
+  var time = new Date();
+  $("#stage").addClass("sky-gradient-"+time.getHours());
+  if (time.getHours() > 15 || time.getHours() < 8) {
+    $("body").css("background-color", "#505050");
+  } else {
+    $("body").css("background-color", "#000");
+  }
+  window.setTimeout(function() {
+      setSky();
+  }, 60000);
+}
+
+function setMenu() {
+  for (var i = 1; i < options.length; i++) {
     var divToPush = $("<div></div>");
-    divToPush.attr("id", "option"+ i);
+    divToPush.attr("id", "option" + i);
     divToPush.attr("class", "menuOption button");
-    divToPush.html("<div class='menuText'>"+options[i]+"</div>");
-    divToPush.attr("onclick", "setStage("+i+")");
+    divToPush.html("<div class='menuText'>" + options[i-1] + "</div>");
+    divToPush.attr("onclick", "setStage(" + i + ")");
     $("#menuContainer").append(divToPush);
   }
-  $(".menuOption").css("visibility","hidden");
+  $(".menuOption").css("visibility", "hidden");
 }
