7f3270e38f8dda604b32e8997ab3f048609e7682
Rahul Sondhi <rahul.sondhi@hotmail.com>
2018-06-14 22:26:06 -0400

    Testing contact json

--- js/parser.js
+++ js/parser.js
@@ -1,12 +1,7 @@
 function setPage() {
-  if (fetchContact()) {
-    if (fetchProjects()) {
-      if (fetchQuestions()) {
-        return fetchResume();
-      }
-    }
-  }
-  return false;
+  console.log("Welcome to my console!");
+  console.log("Loading jsons:");
+  return fetchContact();
 }
 
 function fetchContact() {
@@ -15,9 +10,70 @@ function fetchContact() {
     dataType: "json",
     url: "json/contact.json"
   }).done(function(data) {
-    console.log(data);
-    return true
+    console.log("Contacts Loaded!");
+    var contacts = JSON.parse(data);
+    setContact(contacts)
+    return fetchProjects();
   });
+
+}
+
+function setContact(contacts){
+  var contactsToAdd = questions.questions;
+  for(var i=0; i < 6; i++){
+    var questionDiv = $("<div></div>");
+    questionDiv.attr("class","question");
+    var questionDivText = $("<div></div>");
+    questionDivText.attr("class","questionTest");
+    questionDivText.html(questionsToAdd[i].questionText);
+    var questionDivAnswer = $("<div></div>");
+    questionDivAnswer.attr("class","questionAnswer");
+    questionDivAnswer.html(questionsToAdd[i].questionAnswer);
+    questionDiv.append(questionDivText);
+    questionDiv.append("<hr>");
+    questionDiv.append(questionDivAnswer);
+    if(i<3){
+      $("aboutMeLeft").append(questionDiv);
+    }else{
+      $("aboutMeRight").append(questionDiv);
+    }
+  }
+
+  var emails = contacts.emails;
+  var sites = contacts.sites;
+  var socialMedia = contacts.socialMedia;
+
+  for(var i = 0; i < emails.length;i++){
+    var email = $("<div></div>");
+    email.attr("class","titleContactInfo");
+    email.html(emails[i]);
+
+    $("#emails").append(email);
+  }
+
+  for(var i = 0; i < sites.length;i++){
+    var site = $("<a></a>");
+    site.attr("class","titleSiteInfo");
+    site.attr("onclick","{ window.open('"+site[i].link+"','_blank');}");
+    site.html(site[i].title);
+
+    $("#sites").append(site);
+  }
+
+  for(var i = 0; i < socialMedia.length;i++){
+    var socialLogo = $("<div></div>");
+    socialLogo.attr("class","socialLogo");
+
+    var socialLogoImg = $("<img></img>");
+    socialLogoImg.attr("src",socialMedia[i].img);
+    socialLogoImg.attr("class","socialLogoPic");
+    socialLogoImg.attr("onclick","{window.open('"+socialMedia[i].link+"','_blank');}");
+
+    socialLogo.append(socialLogoImg);
+    $("#contactLeft").append(socialLogo);
+    $("#profileGallery").append(socialLogo)
+  }
+
 
 }
 
@@ -27,8 +83,9 @@ function fetchProjects() {
     dataType: "json",
     url: "json/projects.json"
   }).done(function(data) {
+    console.log("Projects Loaded!");
     console.log(data);
-    return true
+    return fetchQuestions();
   });
 
 }
@@ -39,8 +96,10 @@ function fetchQuestions() {
     dataType: "json",
     url: "json/questions.json"
   }).done(function(data) {
+    console.log("Questions Loaded!");
     var questions = JSON.parse(data);
-    return setQuestion(questions);
+    setQuestion(questions)
+    return fetchResume();
   });
 
 }
@@ -73,8 +132,9 @@ function fetchResume() {
     dataType: "json",
     url: "json/resume.json"
   }).done(function(data) {
+    console.log("Resume Loaded!");
     console.log(data);
-    return true
+    return true;
   });
 
 }
